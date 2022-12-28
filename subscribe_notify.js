98f08ef2b985f91d70b44527d486f6de90d36c75
MaxPuig <45133421+MaxPuig@users.noreply.github.com>
2022-12-28 18:38:47 +0100

    Better error logging

--- subscribe_notify.js
+++ subscribe_notify.js
@@ -16,6 +16,7 @@ app.get('/gwl/delete/:token', async function (req, res) {
     log_events(`Delete request by: ${token}`)
     let topics;
     try {
+        // Get all topics the token is subscribed to, in order to then delete them one by one
         let sub_topics = [];
         let response = await fetch(`https://iid.googleapis.com/iid/info/${token}?details=true`, {
             method: 'GET',
@@ -24,28 +25,33 @@ app.get('/gwl/delete/:token', async function (req, res) {
         response = await response.json();
         topics = response.rel?.topics || [];
         for (let topic in topics) sub_topics.push(topic);
-        if (response.error) topics = "error";
+        if (response.error) {
+            log_events("ERROR retrieving subscribed topics: " + response.error.toString());
+            topics = "error";
+        };
     } catch (e) {
         topics = "error";
-        console.log(e.toString());
+        log_events("ERROR fetching subscribed topics: " + e.toString());
     }
     if (topics == "error") {
         res.send({ success: false });
         return;
     }
+    // Delete all topics the token is subscribed to
     try {
         if (topics) {
             for (let topic in topics) {
-                await fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
+                let responseDelete = await fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
                     method: 'DELETE',
                     headers: { 'Content-Type': 'application/json', 'Authorization': "key=" + key }
                 });
+                if (responseDelete.error) log_events("ERROR deleting topics: " + responseDelete.error.toString());
             }
         }
         res.send({ success: true });
         return;
     } catch (e) {
-        console.log(e.toString());
+        log_events("ERROR fetch deleting topics: " + e.toString());
         res.send({ success: false });
         return;
     }
@@ -57,34 +63,36 @@ app.get('/gwl/subscribe/:token/:address', async function (req, res) {
     const address = req.params.address;
     log_events(`Subscribe request by: ${token}`)
     try {
+        // Subscribe the token to the topic (address)
         let response = await fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${address}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', 'Authorization': "key=" + key }
         });
         response = await response.json();
         if (response.error) {
+            log_events("ERROR subscribing to topic: " + response.error.toString());
             res.send({ success: false });
             return;
         }
         res.send({ success: true });
         return;
     } catch (e) {
-        console.log(e.toString())
+        log_events("ERROR fetch subscribing to topic: " + e.toString());
         res.send({ success: false });
         return;
     }
 });
 
 
-function log_events(event){
+function log_events(event) {
     const dateObject = new Date();
-    const date = (`0 ${dateObject.getDate()}`).slice(-2);
-    const month = (`0 ${dateObject.getMonth() + 1}`).slice(-2);
+    const day = (`0${dateObject.getDate()}`).slice(-2);
+    const month = (`0${dateObject.getMonth() + 1}`).slice(-2);
     const year = dateObject.getFullYear();
-    const hours = dateObject.getHours();
-    const minutes = dateObject.getMinutes();
-    const seconds = dateObject.getSeconds();
-    console.log(`${year}-${month}-${date} ${hours}:${minutes}:${seconds} - ${event}`);
+    const hours = (`0${dateObject.getHours()}`).slice(-2);
+    const minutes = (`0${dateObject.getMinutes()}`).slice(-2);
+    const seconds = (`0${dateObject.getSeconds()}`).slice(-2);
+    console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds} - ${event}`);
 }
 
 
@@ -92,5 +100,5 @@ const httpsServer = https.createServer(options, app);
 
 
 httpsServer.listen(port, () => {
-    console.log("Https server listing on port : " + port)
+    log_events("Https server listing on port: " + port)
 });
